e626f300c53521877358f0000827e84e7ccd7b6d
olliejm <olliejm@protonmail.com>
2019-01-09 11:54:57 +0000

    change formatting

--- ping.js
+++ ping.js
@@ -6,17 +6,18 @@ module.exports = async function (activity) {
     try {
         api.initialize(activity);
 
-        // *todo* create a simple and fast API request
         const response = await api('/me');
 
-        // ping success: when result returns status 200
-        activity.Response.Data = {success: (response && response.statusCode === 200)};
+        activity.Response.Data = {
+            success: (response && response.statusCode === 200)
+        };
     } catch (error) {
-        // in ping errors are returned in regular response with success == false
-        const response = {success: false};
+        const response = {
+            success: false
+        };
 
-        // return error response
         let m = error.message;
+
         if (error.stack) {
             m = m + ': ' + error.stack;
         }
